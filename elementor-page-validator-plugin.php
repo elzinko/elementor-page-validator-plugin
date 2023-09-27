0d7bec374de0d6f4f188cb67e19f72740a3d02ad
Thomas Couderc <thomas.couderc@gmail.com>
2023-09-27 20:45:27 +0200

    demo: temp commit for wp pusher demo

--- elementor-page-validator-plugin.php
+++ elementor-page-validator-plugin.php
@@ -1,34 +1,13 @@
 <?php
-
 /**
  * Plugin Name: Elementor Page Validator Plugin
- * Description: This plugin enable all Elementor pages and blocks validation for a given website.
+ * Description: This plugin enables all Elementor pages and blocks validation for a given website.
  * Version: 1.0
  * Author: Thomas Couderc
  */
 
-
-define('EPVP_PLUGIN_DEBUG', false);
-define('EPVP_PLUGIN_VERSION', '1.0.0');
-define('EPVP_PLUGIN_NAME', 'elementor-page-validator-plugin');
-
-
-/**
- * Register plugin activation hook.
- * Check if Elementor is active, if not, deactivate plugin.
- */
-
-function elementor_page_validation_plugin_activation()
-{
-    if (!is_plugin_active('elementor/elementor.php')) {
-        // Disable your plugin.
-        deactivate_plugins(plugin_basename(__FILE__));
-
-        // Create an error message to let the user know.
-        wp_die(__('This plugin needs Elementor to work. Please install and activate Elementor, then try again.', 'media-info-tracker'));
-    }
-}
-register_activation_hook(__FILE__, 'elementor_page_validation_plugin_activation');
+define('WEBSITE_URL', 'https://yearbook:receptive@roomy-company.localsite.io');
+define('SNAPSHOT_URL', 'https://webshot-elzinko.vercel.app/api/webshot?url='. WEBSITE_URL);
 
 // Add menu in admin panel
 add_action('admin_menu', 'add_menu_page_validator');
@@ -37,6 +16,117 @@ function add_menu_page_validator() {
     add_menu_page('Page validator', 'Page validator', 'manage_options', 'page_validator', 'show_page_validator_plugin');
 }
 
-function show_page_validator_plugin() {
-    echo '<h1>Here is ' . EPVP_PLUGIN_NAME . ' plugin version ' . EPVP_PLUGIN_VERSION . ' !! </h1>';
+function find_first_title($elements) {
+    foreach ($elements as $element) {
+        if (isset($element['widgetType']) && ($element['widgetType'] === 'heading' || $element['widgetType'] === 'widget-menu-anchor')) {
+            return isset($element['settings']['title']) ? $element['settings']['title'] : 'Sans titre';
+        }
+        
+        if (isset($element['elements']) && is_array($element['elements'])) {
+            $title = find_first_title($element['elements']);
+            if ($title) {
+                return $title;
+            }
+        }
+    }
+    return null;
 }
+
+function show_page_validator_plugin() {
+    // Handle form submission
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        if (isset($_POST['valider'])) {
+            foreach ($_POST['valider'] as $page_id) {
+                update_post_meta($page_id, 'is_validated', 'true');
+            }
+        }
+
+        if (isset($_POST['valider_section'])) {
+            foreach ($_POST['valider_section'] as $section_id) {
+                update_post_meta($section_id, 'is_validated', 'true');
+            }
+        }
+    }
+
+    // Fetch all pages
+    $args = array(
+        'post_type' => 'page',
+        'posts_per_page' => -1,
+    );
+    $query = new WP_Query($args);
+
+    echo '<div class="wrap">';
+    echo '<h1>Validation des éléments Elementor</h1>';
+    echo '<form method="post">';
+    echo '<table class="wp-list-table widefat fixed striped">';
+    echo '<thead><tr><th scope="col">ID</th><th scope="col">Valider</th><th scope="col">Fil d\'Ariane</th><th scope="col">Aperçu</th></tr></thead>';
+    echo '<tbody>';
+
+    if ($query->have_posts()) {
+        while ($query->have_posts()) {
+            $query->the_post();
+            $page_id = get_the_ID();
+            $titre_page = get_the_title();
+
+            // Display row for the page
+            echo '<tr data-id="' . $page_id . '">';
+            echo '<td>' . $page_id . '</td>';
+            echo '<td><input type="checkbox" name="valider[]" value="' . $page_id . '"></td>';
+            echo '<td>' . $titre_page . '</td>';
+            echo '<td><img src="' . SNAPSHOT_URL . '&selectorId=' . $page_id . '" width="100"></td>';
+            echo '</tr>';
+
+            // Fetch Elementor data
+            $raw_elementor_data = get_post_meta($page_id, '_elementor_data', true);
+            $elementor_data = json_decode($raw_elementor_data, true);
+
+            // Loop through the data to find sections
+            if (is_array($elementor_data)) {
+                foreach ($elementor_data as $element) {
+                    if (isset($element['elType']) && 'section' === $element['elType']) {
+                        $section_title = 'Section sans titre';  // Default title
+
+                        // Check if the section contains elements
+                        if (isset($element['elements']) && is_array($element['elements'])) {
+                            $title = find_first_title($element['elements']);
+                            if ($title) {
+                                $section_title = $title;
+                            }
+                        }
+
+                        // Display row for the section
+                        echo '<tr data-id="' . $page_id . '-' . $element['id'] . '">';
+                        echo '<td>' . $element['id'] . '</td>';
+                        echo '<td><input type="checkbox" name="valider_section[]" value="' . $element['id'] . '"></td>';
+                        echo '<td>' . $titre_page . ' > ' . $section_title . '</td>';
+                        echo '<td><img src="' . SNAPSHOT_URL  . '&selectorId=' . $element['id'] . '" width="100"></td>';
+                        echo '</tr>';
+                    }
+                }
+            }
+
+            // Display row for the section
+            echo '<tr data-id="' . $page_id . '-' . $element['id'] . '">';
+            echo '<td>' . $element['id'] . '</td>';
+            echo '<td><input type="checkbox" name="valider_section[]" value="' . $element['id'] . '"></td>';
+            echo '<td>' . $titre_page . ' > ' . $section_title . '</td>';
+            echo '</tr>';
+        }
+    }
+
+    echo '</tbody>';
+    echo '</table>';
+    echo '<input type="submit" value="Valider les éléments sélectionnés">';
+    echo '</form>';
+    echo '</div>';
+    echo '<script>
+    jQuery(document).ready(function($) {
+        $("input[name=\'valider[]\']").change(function() {
+            var parentChecked = $(this).prop("checked");
+            var parentId = $(this).val();
+            $("tr[data-id^=\'" + parentId + "-\']").find("input[type=\'checkbox\']").prop("checked", parentChecked);
+        });
+    });
+    </script>';
+}
+?>
